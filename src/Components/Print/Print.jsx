d573f5ef85b39b28626b83c1f2e2d0899405add8
Gemry Bulante <bgemry@yahoo.com>
2020-12-10 23:23:35 +0800

    final2

--- src/Components/Print/Print.jsx
+++ src/Components/Print/Print.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Document, Page, PDFViewer, Text, View, StyleSheet } from '@react-pdf/renderer';
+import React from 'react';
+import { Document, Page, PDFViewer, Text, View } from '@react-pdf/renderer';
  
 function MyApp() {
   return (
